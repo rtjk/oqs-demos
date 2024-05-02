9c67fa421e56813d21fb81c6c6b0544eb9bed6d6
rtjk <47841774+rtjk@users.noreply.github.com>
2024-05-02 13:37:55 +0200

    edit __mytest.sh

--- curl/__mytest.sh
+++ curl/__mytest.sh
@@ -1,5 +1,5 @@
 for i in "CROSSrsdp128balanced" "CROSSrsdp128fast" "CROSSrsdp128small" "CROSSrsdp192balanced" "CROSSrsdp192fast" "CROSSrsdp192small" "CROSSrsdp256balanced" "CROSSrsdp256fast" "CROSSrsdp256small" "CROSSrsdpg128balanced" "CROSSrsdpg128fast" "CROSSrsdpg128small" "CROSSrsdpg192balanced" "CROSSrsdpg192fast" "CROSSrsdpg192small" "CROSSrsdpg256balanced" "CROSSrsdpg256fast" "CROSSrsdpg256small"
 do
-    echo $i
+    echo "\n" $i
     docker run -e TEST_TIME=5 -e KEM_ALG=kyber768 -e SIG_ALG=$i -it oqs-curl perftest.sh
 done
